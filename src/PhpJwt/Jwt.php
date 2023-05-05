c0fe804d28f4895bae799dd3af692362c655a18e
Geraint Hywel <g.s.j.hywel@swansea.ac.uk>
2023-05-05 16:44:48 +0100

    Added a verify() method

--- src/PhpJwt/Jwt.php
+++ src/PhpJwt/Jwt.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace PhpJwt;
 
+use JsonException;
+use OpenSSLAsymmetricKey;
 use PhpJwt\Encoder;
-
-use function PHPUnit\Framework\stringContains;
 
 class Jwt
 {
@@ -20,5 +20,69 @@ class Jwt
         $claims     = new JwtClaimsSet($payload);
         $signature  = new JsonWebSignature(new Encoder\EncoderFactory());
         return $signature->getSignedToken($joseHeader, $claims, $parameters);
+    }
+
+    public static function verify(string $token, array $parameters): bool
+    {
+        $parts = explode('.', $token);
+        if (count($parts) !== 3) {
+            return false;
+        }
+
+        try {
+            $header  = self::decodeHeader($parts[0]);
+            $payload = self::decodePayload($parts[1]);
+        } catch (JsonException $e) {
+            return false;
+        }
+
+        if (str_starts_with($header['alg'], 'HS')) {
+            $expected = self::encode($header, $payload, $parameters);
+            return $expected === $token;
+        }
+
+        if (str_starts_with($header['alg'], 'RS')) {
+            $publicKey = openssl_pkey_get_public($parameters['public_key']);
+            if (!$publicKey instanceof OpenSSLAsymmetricKey) {
+                return false;
+            }
+            $data = "{$parts[0]}.{$parts[1]}";
+            $signature = self::base64UrlDecode($parts[2]);
+            $algorithm = self::getRsAlgorithm($header['alg']);
+            $result = openssl_verify($data, $signature, $publicKey, $algorithm);
+            return $result === 1;
+        }
+    }
+
+    private static function decodeHeader(string $encodedHeader): array
+    {
+        $json = self::base64UrlDecode($encodedHeader);
+        return json_decode($json, true, flags: JSON_THROW_ON_ERROR);
+    }
+
+    private static function decodePayload(string $encodedPayload): array
+    {
+        $json = self::base64UrlDecode($encodedPayload);
+        return json_decode($json, true, flags: JSON_THROW_ON_ERROR);
+    }
+
+    private static function base64UrlDecode(string $data): string
+    {
+        $urlUnsafeData = strtr($data, '-_', '+/');
+        $paddedData = str_pad($urlUnsafeData, strlen($data) % 4, '=', STR_PAD_RIGHT);
+        return base64_decode($paddedData);
+    }
+
+    private static function getRsAlgorithm(string $key): int
+    {
+        $algorithms = [
+            'RS256' => OPENSSL_ALGO_SHA256,
+            'RS384' => OPENSSL_ALGO_SHA384,
+            'RS512' => OPENSSL_ALGO_SHA512,
+        ];
+        if (array_key_exists($key, $algorithms)) {
+            return $algorithms[$key];
+        }
+        throw new Exception("Unrecognised algorithm '{$key}'");
     }
 }
--- tests/PhpJwt/JwtTest.php
+++ tests/PhpJwt/JwtTest.php
@@ -54,4 +54,65 @@ class JwtTest extends TestCase
             ],
         ];
     }
+
+    /**
+     * @test
+     * @dataProvider verifyProvider
+     */
+    public function canVerify(string $token, array $parameters, bool $expected): void
+    {
+        $actual = Jwt::verify($token, $parameters);
+        $this->assertSame($expected, $actual);
+    }
+
+    public static function verifyProvider(): array
+    {
+        return [
+            'a completely corrupt token' => [
+                'token'      => 'this is not a valid token',
+                'parameters' => [],
+                'expected'   => false,
+            ],
+            'a valid(-ish) looking token, but actually invalid' => [
+                'token'      => 'A.B.C',
+                'parameters' => [],
+                'expected'   => false,
+            ],
+            'a valid HS256 token' => [
+                'token'      => 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvZSBCbG9nZ3MiLCJpYXQiOjE1MTYyMzkwMjJ9.cWPf6g4AMgzx5CorjSp8bK1ywXIW5o2dM7bBdUMHhlw',
+                'parameters' => [ 'secret' => 'my top secret' ],
+                'expected'   => true,
+            ],
+            'a valid HS256 token but the wrong shared secret' => [
+                'token'      => 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvZSBCbG9nZ3MiLCJpYXQiOjE1MTYyMzkwMjJ9.cWPf6g4AMgzx5CorjSp8bK1ywXIW5o2dM7bBdUMHhlw',
+                'parameters' => [ 'secret' => 'the wrong secret' ],
+                'expected'   => false,
+            ],
+            'a HS256 token with a bad signature' => [
+                'token'      => 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvZSBCbG9nZ3MiLCJpYXQiOjE1MTYyMzkwMjJ9.fOoBaR',
+                'parameters' => [ 'secret' => 'my top secret' ],
+                'expected'   => false,
+            ],
+            'a valid RS256 token' => [
+                'token'      => 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvZSBCbG9nZ3MiLCJpYXQiOjE1MTYyMzkwMjJ9.EdE3Xc4_911RAnoVdz9hPwyAWHaKfZ14n7BZRq_0MFlPN_Dc7zhABI8UVBb8cDYLm7w-yV1CiPt05MLgt3l6TYPIJDMAdtcKJa9V1-hBQGCveTOqtt1-wct1Htd5ODJ3HYVEWsBe9s2OGcW3dvY8P-1ZgPaRj4LxGNf46_eB-XnEfscnCi5GdmkyI9kL2N_1SePaC_b_sKwG_WoIPttGlZNczBLZdkB2_1Eb6akmefL6LjM-J4mS9vBm11bYV0BMGNpQBTbjPsUblUjuiXT1f2-C_L_k8JusiTaPpqHyAk2tsHp44TTt4HvjOjufaPDioeGGpALNj3g522yeqmQUbg',
+                'parameters' => [ 'public_key' => file_get_contents(__DIR__ . '/../public_key.pem') ],
+                'expected'   => true,
+            ], 
+            'a valid RS256 token, but the public key is invalid' => [
+                'token'      => 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvZSBCbG9nZ3MiLCJpYXQiOjE1MTYyMzkwMjJ9.EdE3Xc4_911RAnoVdz9hPwyAWHaKfZ14n7BZRq_0MFlPN_Dc7zhABI8UVBb8cDYLm7w-yV1CiPt05MLgt3l6TYPIJDMAdtcKJa9V1-hBQGCveTOqtt1-wct1Htd5ODJ3HYVEWsBe9s2OGcW3dvY8P-1ZgPaRj4LxGNf46_eB-XnEfscnCi5GdmkyI9kL2N_1SePaC_b_sKwG_WoIPttGlZNczBLZdkB2_1Eb6akmefL6LjM-J4mS9vBm11bYV0BMGNpQBTbjPsUblUjuiXT1f2-C_L_k8JusiTaPpqHyAk2tsHp44TTt4HvjOjufaPDioeGGpALNj3g522yeqmQUbg',
+                'parameters' => [ 'public_key' => 'this is not a valid public key' ],
+                'expected'   => false,
+            ], 
+            'a valid RS384 token' => [
+                'token'      => 'eyJhbGciOiJSUzM4NCIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvZSBCbG9nZ3MiLCJpYXQiOjE1MTYyMzkwMjJ9.YKPrlYE64n_KG5DjL83Q6LZEOJ3MFK4YHti4N10K068s10GDxhlrYfpkUJjkhZ_7AhT5KRBfEDZ__9AMngXhcTF0QzK14BtvXe8AL6jy5goX3-0LT0HLiqwVU691NKXF-qHiCE6ydSPV0gsvnLNLeAeP7AC1MEoy77IMZxBi0ifvoNsD5qkw5Dw7sk10QG1ASN21PiBDQ-wZzIcKJ6m5wGnw5QLkG-FKLxahm6kqHnkaCPIzgQkeI2BUwVHpjRXXjEIR6sPaqfHSWbzJrLbMAKyOBRgLFM3mk6WEYWrrXdAMoM8LESgRTvJbKdw6-9jrDsjeuHYABp4TBn2u82wSUQ',
+                'parameters' => [ 'public_key' => file_get_contents(__DIR__ . '/../public_key.pem') ],
+                'expected'   => true,
+            ], 
+            'a valid RS512 token' => [
+                'token'      => 'eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvZSBCbG9nZ3MiLCJpYXQiOjE1MTYyMzkwMjJ9.I5Kr-zuHEEt2Dp6zXMmm6U1Gw4-S-PjaBUIAZXk_FA1Di6Fa-BO_3OAJ44cBfFoywG1oeCGiOvSsN3uR6oGkEWT3ZYOXZr51fqi_r4mB1REQoorNiYdkcxTKJLEFRwZi-P7cj_i5wBjSXyraY5-rPsXSYck4W03y63mRaKZLYUBNPl9SMdn0UqZYEz7tMTupJvuQ5dellFOGllrKOSIhLRHStwiEehPiNFAPF-WhVY9_EDHcXB4lfN8SCxYAOsZbSCHFi_CryrycGBlbPeaDITVjpwLgx2Gpm9UhZus1LFK-l_wLDJRPcZSUj2oQqe71sX8it0RgwRqnRzqhtgKQSA',
+                'parameters' => [ 'public_key' => file_get_contents(__DIR__ . '/../public_key.pem') ],
+                'expected'   => true,
+            ], 
+        ];
+    }
 }
